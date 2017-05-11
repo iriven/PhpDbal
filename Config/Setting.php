f6fd32e2baa07c00657bf94292d6e7c0bea7b983
Alfred Tchondjo <iriven@yahoo.fr>
2017-05-11 10:17:51 +0200

    Update Setting.php

--- Config/Setting.php
+++ Config/Setting.php
@@ -1,1 +1,25 @@
 <?php
+return  array (
+      'default' =>
+          array (
+              'driver' => 'mysql',
+              'host' => 'localhost1',
+              'dbname' => 'mydatabase1',
+              'user' => 'myusername1',
+              'password' => 'mypassword',
+              'prefix'=>'DB1_',
+              'port' => 3306,
+              'persistent' => 1,
+              'fetchmode' => 'object',
+              'prepare' => 1
+          ),
+      'project2' =>
+          array (
+              'driver' => 'pgsql',
+              'host' => 'localhost2',
+              'dbname' => 'mydatabase2',
+              'user' => 'myusername2',
+              'password' => 'mypassword',
+              'port' => 5432
+          )
+  );
